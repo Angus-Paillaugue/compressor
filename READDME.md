bb2515e2f5c2a66f7115571299fb402e5296fef2
Angus-Paillaugue <angus.paillaugue40@gmail.com>
2024-07-02 19:03:27 +0200

    Updated script and added a README

---
+++ READDME.md
@@ -0,0 +1,32 @@
+
+<br/>
+<div align="center">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/FFmpeg_icon.svg/1200px-FFmpeg_icon.svg.png" alt="Logo" width="80" height="80">
+  <h3 align="center">Automatic Video Compressor</h3>
+</div>
+
+## Introduction
+This simple bash script is used to compress large amount of videos in a directory.
+<div align="center">
+  <video src="./ressources/exemple.mp4" width="676" height="532" controls></video>
+</div>
+
+## Getting Started
+### Prerequisites
+You ned to have Ffmpeg installed on your system (Follow the installation guides here)
+
+### Usage
+To do so, just call the script and pass the directory with your videos an an input using the `-i` flag (ex : `./compressor.sh -i /path/to/your/videos`)
+
+```sh
+Usage: <script> -inputPath <inputPath> [options]
+
+Options:
+  -i, -inputPath <inputPath>  Specify the input path.
+  -preset <presetValue>       Specify the preset value (default: fast).
+                              Valid presets: ultrafast, superfast, veryfast,
+                              faster, fast, medium, slow, slower, veryslow, placebo.
+  -crf <value>                Specify the CRF value (default: 23).
+                              Valid range: 0-51.
+  -h, --help                  Display this help message and exit.
+```
--- compressor.sh
+++ compressor.sh
@@ -1,11 +1,75 @@
 #!/bin/bash
 
-inputPath=$1
-
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
+
+
+# Function to display help
+function displayHelp() {
+  echo "Usage: compress-copy.sh -inputPath <inputPath> [options]"
+  echo ""
+  echo "Options:"
+  echo "  -i, -inputPath <inputPath>  Specify the input path."
+  echo "  -preset <presetValue>       Specify the preset value (default: fast)."
+  echo "                              Valid presets: ultrafast, superfast, veryfast, faster, fast, medium, slow, slower, veryslow, placebo."
+  echo "  -crf <value>                Specify the CRF value (default: 23)."
+  echo "                              Valid range: 0-51."
+  echo "  -h, --help                  Display this help message and exit."
+}
+
+# Flags
+preset="fast" # Default value for the preset
+valid_presets=("ultrafast" "superfast" "veryfast" "faster" "fast" "medium" "slow" "slower" "veryslow" "placebo")
+crf=23
+inputPath=""
+
+# Parse the arguments
+while [ "$1" != "" ]; do
+  case $1 in
+    -h | --help )
+      displayHelp
+      exit 0
+      ;;
+    -i | -inputPath )
+      shift
+      inputPath=$1
+      ;;
+    -preset )
+      shift
+      preset=$1
+      ;;
+    -crf )
+      shift
+      crf=$1
+      ;;
+    *)
+      echo -e "${RED}Unknown option$NC: $1"
+      display_help
+      exit 1
+  esac
+  shift
+done
+
+# Args validdate
+# Validate inputPath
+if [ -z "$inputPath" ]; then
+  echo -e "${RED}Error:$NC Input path is required."
+  displayHelp
+  exit 1
+fi
+# Validate preset
+if [[ ! " ${valid_presets[@]} " =~ " ${preset} " ]]; then
+  echo -e "${RED}Error:$NC Invalid preset value '$preset'. Valid options are: ${valid_presets[*]}"
+  exit 1
+fi
+# Validate crf
+if [[ ! $crf =~ ^[0-9]+$ ]] || [ $crf -lt 0 ] || [ $crf -gt 51 ]; then
+  echo -e "${RED}Error:$NC Invalid crf value '$crf'. crf value should be an integer between 0 and 51"
+  exit 1
+fi
+
 
 # Error handling
 if [ -z "$inputPath" ]; then
@@ -39,7 +103,9 @@ function spinner() {
 
 }
 
+# Main script
 start=`date +%s`
+# ! You can add more video formats by adding `-o -iname "*.extension"`
 files=$(find "$inputPath" -maxdepth 1 -type f \( -iname "*.mp4" -o -iname "*.mov" \))
 numberOfFiles=$(echo "$files" | wc -l)
 # If no files found in $inputPath, exit
@@ -47,9 +113,9 @@ if [ $numberOfFiles -eq 0 ]; then
   echo -e " $RED✗$NC No videos found in $inputPath"
   exit 1
 fi
-echo -e " $GREEN✓$NC Found $numberOfFiles files to compress"
+echo -e " $GREEN✓$NC Found $numberOfFiles videos to compress"
 originalDirSizeHR=$(du -h -s "$inputPath" | cut -f1)
-originalDirSize=$(du -s "$inputPath")
+originalDirSize=$(du -s "$inputPath" | cut -f1)
 
 filesToGo=$numberOfFiles
 # Loop through files in inputPath
@@ -66,7 +132,7 @@ for file in $files; do
     baseSize=$(du -h "$file" | cut -f1)
 
     # Actual compression
-    ffmpeg -i "$file" -vcodec libx265 -crf 23 -preset fast "$outputName" > /dev/null 2>&1 &
+    ffmpeg -i "$file" -vcodec libx265 -crf $crf -preset "$preset" "$outputName" > /dev/null 2>&1 &
     videoPid=$!
     # Displays the spinner and waits for the videoPid to finish
     spinner "Compressing $fileName ($RED$baseSize$NC)" &
@@ -80,7 +146,7 @@ for file in $files; do
     rm "$file"
     mv "$outputName" "$file"
     filesToGo=$((filesToGo-1))
-    echo -e " $GREEN✓$NC $fileName : $RED$baseSize$NC → $GREEN$newSize$NC ($YELLOW-$filesToGo$NC to go)"
+    echo -e " $GREEN✓$NC $fileName : $RED$baseSize$NC → $GREEN$newSize$NC ($YELLOW$filesToGo$NC to go)"
   fi
 done
 
@@ -101,6 +167,6 @@ echo "$S seconds"
 
 # Calculates and prints the compression rate
 finalDirSizeHr=$(du -h -s "$inputPath" | cut -f1)
-finalDirSize=$(du -s "$inputPath")
-compressionRate=$(echo "scale=2; ($originalDirSize - $finalDirSize) / $originalSize * 100" | bc)
+finalDirSize=$(du -s "$inputPath" | cut -f1)
+compressionRate=$(echo "scale=1; ($originalDirSize - $finalDirSize) / $originalDirSize * 100" | bc)
 echo -e " $GREEN✓$NC Reduced the directory size by $YELLOW$compressionRate$NC% : $RED$originalDirSizeHR$NC → $GREEN$finalDirSizeHr$NC"
